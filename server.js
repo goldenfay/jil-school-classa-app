6b54c25ed8919de2eee5c5a4edf678b029108dda
goldenfay <selmanefaycal@hotmail.fr>
2020-12-16 15:48:13 +0100

    added remote DB connection setting

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ server.use(bodyParser.urlencoded({ extended: false }));
 server.use(express.static("uploads"));
 
 const corsConfig = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Methods",
