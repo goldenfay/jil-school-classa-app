7c2af1ca1d860ecd320824bdaf21ff8427eb7313
goldenfay <selmanefaycal@hotmail.fr>
2020-12-16 16:57:38 +0100

    change api url

--- src/fake/server/index.js
+++ src/fake/server/index.js
@@ -9,7 +9,7 @@ const fileUpload = require('express-fileupload');
 app.use(bodyParser.json());
 
 const corsConfig = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
