f82aefba6cd9ed81bcc2afc441ac5a3aa5860c54
goldenfay <selmanefaycal@hotmail.fr>
2020-12-18 11:10:27 +0100

    added statistic routes

--- statistics/routes/index.js
+++ statistics/routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const elevesStatisticsRouter = require("./eleves-related");
 const profsStatisticsRouter = require("./profs-related");
+const generalRouter = require("./general");
 
 router.use("/eleves", elevesStatisticsRouter);
 router.use("/enseignants", profsStatisticsRouter);
+router.use("/", generalRouter);
 
 module.exports = router;
