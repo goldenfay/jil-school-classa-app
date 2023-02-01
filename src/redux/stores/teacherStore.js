6bf58146abc09c7610b60f51d924cd8f127991a9
goldenfay <46852902+goldenfay@users.noreply.github.com>
2023-02-01 18:20:30 +0100

    Fix redux extension issue

--- src/redux/stores/teacherStore.js
+++ src/redux/stores/teacherStore.js
@@ -2,6 +2,6 @@ import { createStore,compose,applyMiddleware } from "redux";
 import thunk from'redux-thunk'
 import {TeacherReducer} from "../reducers/";
 
-const store = createStore(TeacherReducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(TeacherReducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose));
 
 export default store;
