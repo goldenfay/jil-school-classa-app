f0f688f1d71d43759b717da5f18ff64b6bdac0c5
goldenfay <46852902+goldenfay@users.noreply.github.com>
2023-02-01 18:22:39 +0100

    Fix redux extension issue

--- src/redux/stores/adminStore.js
+++ src/redux/stores/adminStore.js
@@ -2,6 +2,6 @@ import { createStore,compose,applyMiddleware } from "redux";
 import thunk from'redux-thunk'
 import {AdminReducer} from "../reducers/";
 
-const store = createStore(AdminReducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(AdminReducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose));
 
 export default store;
